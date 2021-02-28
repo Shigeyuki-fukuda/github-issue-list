63c5324042517ece7a072076e796cd62f56399ba
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-02-28 15:48:32 +0900

    feat: /issues/:numberでアクセスするissue詳細コンポーネントのひな型を作成

---
+++ pages/issues/_number.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <p>
+      Issue ID: {{ issueNumber }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      issueNumber: this.$route.params.number
+    }
+  }
+}
+</script>
