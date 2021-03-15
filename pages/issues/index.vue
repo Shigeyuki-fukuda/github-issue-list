72478847fb7deffb08b41cca4fdda69b877179da
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-16 08:29:22 +0900

    refactor: 一覧のデータ取得でmountedを使わずにasyncDataを使用するようにした

--- pages/issues/index.vue
+++ pages/issues/index.vue
@@ -96,17 +96,11 @@ import Vue from 'vue'
 // Vue.extendをすることでTypeScriptではなくVue.jsの記法そのままで書ける
 // refs: https://qiita.com/shindex/items/a90217b9e4c03c5b5215
 export default Vue.extend({
-  data () {
-    return {
-      issues: []
-    }
-  },
-  async mounted () {
+  async asyncData({ app }) {
     // 本来は下記APIを叩くがAPIのリクエスト制限を考慮して開発時は同様の形式のレスポンスのdb.jsonのエンドポイントを叩く
-    // this.issues = await this.$axios.$get('https://api.github.com/repos/facebook/react/issues?page=1&per_page=10')
-    this.issues = await this.$axios.$get('http://localhost:3001/issues')
-    console.log(this.issues[0])
-    console.log(this.issues[1])
+    // const issues = await app.$axios.$get('https://api.github.com/repos/facebook/react/issues?page=1&per_page=10')
+    const issues = await app.$axios.$get('http://localhost:3001/issues')
+    return { issues }
   }
 })
 </script>
