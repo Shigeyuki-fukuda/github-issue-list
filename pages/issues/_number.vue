94dea8fcc65ddb6191af61dc43b0c3592517728a
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-19 10:12:54 +0900

    feat: カスタムカラーを追加してUIに適用

--- pages/issues/_number.vue
+++ pages/issues/_number.vue
@@ -3,7 +3,7 @@
   <div class="w-full md:max-w-3xl mx-auto pt-20">
     <div class="w-full px-4 md:px-8 text-xl text-gray-800 leading-normal">
 			<nuxt-link to="/issues">
-        <p class="text-base md:text-sm text-purple-500 font-bold">&lt; <span class="text-base md:text-sm text-purple-500 font-bold no-underline hover:underline">BACK TO ISSUES</span></p>
+        <p class="text-base md:text-md text-hexadecimal-code hover:text-light-hexadecimal-code font-bold">&lt; BACK TO ISSUES</p>
       </nuxt-link>
       <h1 class="font-bold font-sans break-normal text-gray-900 pt-8 pb-4 text-3xl md:text-4xl">{{ issue.title }} <span class="font-light text-gray-500">#{{ issue.number }}</span></h1>
       <p class="mt-8">
