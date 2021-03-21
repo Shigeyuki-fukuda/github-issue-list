7c5a272cd640372bfd315da42568743e56262200
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-21 14:14:13 +0900

    chore: npm install @nuxtjs/composition-ap

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/composition-api'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
