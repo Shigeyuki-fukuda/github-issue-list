774efc359f6e976fbd2f4b3b91235dcff03a5e32
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-23 00:03:30 +0900

    feat：Tableコンポーネントを追加

---
+++ components/issues/Table.vue
@@ -0,0 +1,48 @@
+<template>
+  <table class="min-w-full leading-normal">
+    <thead>
+      <tr>
+        <th scope="col" class="px-5 py-3 bg-hexadecimal-code border-b border-gray-200 text-white text-left text-sm font-bold">
+          Number
+        </th>
+        <th scope="col" class="px-5 py-3 bg-hexadecimal-code border-b border-gray-200 text-white text-left text-sm font-bold">
+          Title
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="issue in issues" :key="issue.number">
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div class="flex items-center">
+            <div>
+              <p class="text-gray-900 font-bold">
+                #{{ issue.number }}
+              </p>
+            </div>
+          </div>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <nuxt-link :to="`/issues/${issue.number}`">
+            <p class="text-gray-900 hover:text-gray-400 font-bold">
+              {{ issue.title }}
+            </p>
+          </nuxt-link>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'IssuesTable',
+  props: {
+    issues: {
+      type: Array,
+      default: []
+    }
+  }
+})
+</script>
