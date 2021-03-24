f6891f8661addce04370e02fe74b9aadb2f08962
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-24 20:52:33 +0900

    feat: トップページにTailwind CSSを適用してissue一覧へのリンクを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,16 @@
 <template>
-  <div class="container">
+  <div class="m-auto flex justify-center items-center text-center min-h-screen">
     <div>
-      <Logo />
-      <h1 class="title">
-        github-issue-list
+      <h1 class="block font-light text-8xl">
+        GitHub Issue List
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+      <nuxt-link
+          to="/issues"
+          type="button"
+          class="py-4 px-8 mt-8 text-2xl bg-hexadecimal-code hover:bg-light-hexadecimal-code focus:ring-light-hexadecimal-code focus:ring-offset-light-hexadecimal-code text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        Enter
+      </nuxt-link>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
