a2163af2631ba6ed77375dad8aec90f04320ef32
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-24 21:04:44 +0900

    chore: npm run lint-fix

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
         GitHub Issue List
       </h1>
       <nuxt-link
-          to="/issues"
-          type="button"
-          class="py-4 px-8 mt-8 text-2xl bg-hexadecimal-code hover:bg-light-hexadecimal-code focus:ring-light-hexadecimal-code focus:ring-offset-light-hexadecimal-code text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
-        >
+        to="/issues"
+        type="button"
+        class="py-4 px-8 mt-8 text-2xl bg-hexadecimal-code hover:bg-light-hexadecimal-code focus:ring-light-hexadecimal-code focus:ring-offset-light-hexadecimal-code text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+      >
         Enter
       </nuxt-link>
     </div>
