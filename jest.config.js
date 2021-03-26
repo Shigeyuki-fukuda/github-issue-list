db268d689de6e16f9d19ed49ce66799afc98c7c6
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-26 23:47:00 +0900

    chore: npm run lint-fix

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+      tsconfig: 'tsconfig.json'
+    }
   },
   moduleNameMapper: {
     '@nuxtjs/composition-api': '@nuxtjs/composition-api/lib/entrypoint.js',
